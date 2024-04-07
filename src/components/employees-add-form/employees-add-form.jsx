c68ec71b246a40a1e2b240164fd93b9bdc579f75
kasim <kasimsarman@gmail.com>
2024-04-07 14:14:49 +0600

    usestate

--- src/components/employees-add-form/employees-add-form.jsx
+++ src/components/employees-add-form/employees-add-form.jsx
@@ -1,20 +1,35 @@
 import './employees-add-form.css';
+import { useState } from 'react';
 
 function EmployeesAddForm() {
+    const [name, setName] = useState('')
+    const [salary, setSalary] = useState('')
+
     return (
         <div className="app-add-form">
             <h3>Добавьте нового сотрудника</h3>
             <form
+                onSubmit={(e) => {
+                    e.preventDefault()
+                }}
                 className="add-form d-flex">
                 <input type="text"
+                    value={name}
+                    onChange={(e) => {
+                        setName(e.target.value)
+                    }}
                     className="form-control new-post-label"
                     placeholder="Как его зовут?" />
                 <input type="number"
+                    value={salary}
+                    onChange={(e) => {
+                        setSalary(e.target.value)
+                    }}
                     className="form-control new-post-label"
                     placeholder="З/П в $?" />
 
                 <button type="submit"
-                        className="btn btn-outline-light">Добавить</button>
+                    className="btn btn-outline-light">Добавить</button>
             </form>
         </div>
     )
