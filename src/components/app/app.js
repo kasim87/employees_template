b07b49322b8ee501751d0e60b95ca4805bbbd460
unknown <kasimsarman@gmail.com>
2026-06-30 08:41:28 +0500

    change EmployeesList and EmployeesListItemt

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,23 +1,29 @@
-import AppInfo from '../app-info/app-info';
-import SearchPanel from '../search-panel/search-panel';
-import AppFilter from '../app-filter/app-filter';
-import EmployeesList from '../employees-list/employees-list';
-import EmployeesAddForm from '../employees-add-form/employees-add-form';
+import AppInfo from "../app-info/app-info";
+import SearchPanel from "../search-panel/search-panel";
+import AppFilter from "../app-filter/app-filter";
+import EmployeesList from "../employees-list/employees-list";
+import EmployeesAddForm from "../employees-add-form/employees-add-form";
 
-import './app.css';
+import "./app.css";
 
 function App() {
+  const data = [
+    { name: "KASIM", salary: 1000000, increase: true },
+    { name: "TORE", salary: 200000, increase: false },
+    { name: "HAMZA", salary: 10000000, increase: false },
+  ];
+
   return (
     <div className="app">
-        <AppInfo />
+      <AppInfo />
 
-        <div className="search-panel">
-            <SearchPanel/>
-            <AppFilter/>
-        </div>
-        
-        <EmployeesList/>
-        <EmployeesAddForm/>
+      <div className="search-panel">
+        <SearchPanel />
+        <AppFilter />
+      </div>
+
+      <EmployeesList data={data} />
+      <EmployeesAddForm />
     </div>
   );
 }
